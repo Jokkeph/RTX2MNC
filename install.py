a78838c00f7a4c499ab8684c3b015bb5cab5a665
Jokkeph <Jokkeph@hotmail.com>
2018-01-22 16:52:00 +0100

    Updated files description

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
-import os
+import os, sys, shutil
 
-# with is like your try .. finally block in this case
+
 with open('Functions/main.py', 'r') as file:
     # read a list of lines into data
     data = file.readlines()
@@ -17,8 +17,26 @@ with open('/usr/local/bin/universal', 'w') as file:
     file.writelines( data )
 
 os.system("chmod +x /usr/local/bin/universal")
+text = raw_input("Should i install the newest version of DCMTK?(y/n)Takes a while(20 min+)\n")  # Python 2
+if(text == "y" or text == "Y"):
+    os.system("git clone https://github.com/DCMTK/dcmtk")
+    os.system("./dcmtk/configure")
+    os.system("make all")
+    os.system("sudo make install-all")
 
-os.system("mkdir build && cd build")
-os.chdir("rtx2mnc/build")
+text = raw_input("Should i install MINC toolkit 1.9.16 from 20170529?(y/n)Takes around 5-10 min\n")
+if(text == "y" or text == "Y"):
+    os.system("wget http://packages.bic.mni.mcgill.ca/minc-toolkit/Debian/minc-toolkit-1.9.16-20170529-Ubuntu_16.04-x86_64.deb")
+    os.system("echo 'export PATH=$PATH:/opt/minc/bin' >> ~/.bash_profile")
+    os.system("echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/minc/lib' >> ~/.bash_profile")
+    os.system("source ~/.bash_profile")
+    os.remove("minc-toolkit-1.9.16-20170529-Ubuntu_16.04-x86_64.deb")
+
+if(os.path.isdir("rtx2mnc/build")):
+    shutil.rmtree("rtx2mnc/build")
+os.system("mkdir rtx2mnc/build")
+os.chdir(cwd + "/rtx2mnc/build")
 os.system("cmake ..")
 os.system("make && sudo make install")
+
+print("Install finished\nTo use the command write 'universal' to get help write 'universal --help'")
